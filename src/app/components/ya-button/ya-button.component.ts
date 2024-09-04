30d550280911f40f607bced15bfcf82b94551b5a
fiche770 <yossappell@gmail.com>
2024-09-04 16:56:14 +0300

    add ya-button

---
+++ src/app/components/ya-button/ya-button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ya-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './ya-button.component.html',
+  styleUrl: './ya-button.component.scss'
+})
+export class YaButtonComponent {
+
+}
