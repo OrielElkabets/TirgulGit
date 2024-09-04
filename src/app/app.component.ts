0a516b416ad29c8e2b3c046be9e27b113131716a
Oriel Elkabets <orielelkabets@gmail.com>
2024-09-04 17:10:09 +0300

    fix some bugs and add all the components to app-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,17 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { OeButtonComponent } from "./components/oe-button/oe-button.component";
+import { ElazarComponent } from "./components/elazar/elazar.component";
+import { YairComponent } from "./components/yair/yair.component";
+import { YaButtonComponent } from "./components/ya-button/ya-button.component";
+import { RafyButtonComponent } from "./components/rafy-button/rafy-button.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [CommonModule, RouterOutlet, OeButtonComponent]
+    imports: [CommonModule, RouterOutlet, OeButtonComponent, ElazarComponent, YairComponent, YaButtonComponent, RafyButtonComponent]
 })
 export class AppComponent {
   title = 'TirgulGit';
